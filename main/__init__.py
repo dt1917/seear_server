f5f3c40efc107917a7fbaebd4ed56178754c8183
linho1150 <56428918+Linho1150@users.noreply.github.com>
2022-04-03 19:28:53 +0900

    Feat : Main 앱 추가

---
+++ main/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from .views.scrapping import Scheduler
+from .views.tts import TTS
+import json
+
+app=Flask(__name__)
+
+#스케줄링 시작
+schedul=Scheduler()
+#schedul.startScheduler()
+
+@app.route("/")
+def home():
+    data='''
+    {
+      "newsTitle": "힘들겠다",
+      "newsURL": "www.naver.com"
+    }
+    '''
+    res=app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return res
+
+@app.route("/voice")
+def doTTS():
+    tmpTTS=TTS(1234,"안녕하세요")
+    return tmpTTS.speak()
