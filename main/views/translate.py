d5a85e23632b17f5fd5df063d2ba123ae4c9e9e1
Lee Jae Jun <linho301150@gmail.com>
2022-04-07 19:21:42 +0900

    Init : PaPago API 연결

---
+++ main/views/translate.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import json
+import urllib.request
+
+#config ----------------------
+client_id = ""
+client_secret = ""
+
+class papagoAPI:
+    target=""
+    def __init__(self,target):
+        self.target=target
+    
+    def setTarget(self,target):
+        self.target=target
+        
+    def eNtokR(self):
+        try:
+            encText = urllib.parse.quote(self.target)
+            data = "source=en&target=ko&text=" + encText
+            url = "https://openapi.naver.com/v1/papago/n2mt"
+            request = urllib.request.Request(url)
+            request.add_header("X-Naver-Client-Id",client_id)
+            request.add_header("X-Naver-Client-Secret",client_secret)
+            response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+            rescode = response.getcode()
+
+            if(rescode==200):
+                response_body = json.loads(response.read().decode('utf-8'))
+                return response_body['message']['result']['translatedText']
+            else:
+                raise rescode
+        except Exception as e:
+            return e
